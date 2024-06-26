49e62d41755a128431bacaf116dc6aab0159594e
christiankso <christianksoh@gmail.com>
2024-06-25 18:14:24 -0700

    Install pacakges

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas as pandas
+import numpy as np
+import matplotlib as mpl
+
 
 
 def print_hi(name):
